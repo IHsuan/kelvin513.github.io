58cd332462ab251b5b664368577abb9cc2ffae82
Kelvin Lin <kelvin@backer-founder.com>
2016-03-12 13:27:32 +0800

    tab init

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,4 +1,22 @@
 $('.nav-tabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+console.log('ya');
+
+$(window).load(function(){
+
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+		console.log(e.target);
+		setTimeout(function(){
+	  	$($(e.target).attr('href')).children().addClass('pop-active');
+		},100);
+	});
+
+	$('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
+		console.log(e.target);
+	  	$($(e.target).attr('href')).children().removeClass('pop-active');
+	});
+
+});
