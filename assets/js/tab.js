8734c91a310cdba60487e07d868220615e115f75
Kelvin Lin <kelvin@backer-founder.com>
2016-03-14 13:53:38 +0800

    new background

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -3,19 +3,19 @@ $('.nav-tabs a').click(function (e) {
   $(this).tab('show');
 });
 
-console.log('ya');
+// console.log('ya');
 
 $(window).load(function(){
 
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-		console.log(e.target);
+		// console.log(e.target);
 		setTimeout(function(){
-	  	$($(e.target).attr('href')).children().addClass('pop-active');
+		  	$($(e.target).attr('href')).children().addClass('pop-active');
 		},100);
 	});
 
 	$('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
-		console.log(e.target);
+		// console.log(e.target);
 	  	$($(e.target).attr('href')).children().removeClass('pop-active');
 	});
 
