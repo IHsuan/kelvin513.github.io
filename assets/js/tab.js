e522e59e226891e1a4c4d861ba3bcb87eed86e74
Adele Yu <fhead17@gmail.com>
2016-03-19 23:17:53 +0800

    try to fix chat js problem

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,22 +1,29 @@
-$('.nav-tabs a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
 
 // console.log('ya');
 
 $(window).load(function(){
 
-	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-		// console.log(e.target);
+	$('.nav-tabs a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+		chat = $(this).attr('href');
 		setTimeout(function(){
-		  	$($(e.target).attr('href')).children().addClass('pop-active');
+			$(".ChatLog__entry").removeClass('pop-active');
+			$(chat).children().addClass('pop-active');;
 		},100);
 	});
 
-	$('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
-		// console.log(e.target);
-	  	$($(e.target).attr('href')).children().removeClass('pop-active');
-	});
+
+// 	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+// 		// console.log(e.target);
+// 		setTimeout(function(){
+// 		  	$($(e.target).attr('href')).children().addClass('pop-active');
+// 		},100);
+// 	});
+
+// 	$('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
+// 		// console.log(e.target);
+// 	  	$($(e.target).attr('href')).children().removeClass('pop-active');
+// 	});
 
 });
