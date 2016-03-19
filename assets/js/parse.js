b97885c91d5570152114c22e0565b640f3a1d3ff
Adele Yu <fhead17@gmail.com>
2016-03-19 22:40:01 +0800

    fetch json for progress

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -16,4 +16,9 @@ $(function(){
     $("#law-bar .progress-bar-fill").css("width", law_bar_progress);
     $("#firefight-bar .progress-bar-fill").css("width", firefight_bar_progress);
   });
+  var backme_api = "https://taiwanbar.backme.tw/api/projects/219?token=02e8a88dbdc1bdb87f85cb48e82e9f9d";
+  $.getJSON( backme_api ).done(function(data){
+    var progress = data["money_pledged"] / data["money_goal"];
+    $(".progress-bar").css("width", progress)
+  });
 });
