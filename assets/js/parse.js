9048b8532d3f50ef35ea3788673b3dc6e8e97ee2
Kelvin Lin <kelvin@backer-founder.com>
2016-03-20 18:42:13 +0800

    remove tab.js

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -15,10 +15,8 @@ $(function(){
     $("#economic-bar .progress-bar-fill").css("width", economic_bar_progress);
     $("#law-bar .progress-bar-fill").css("width", law_bar_progress);
     $("#firefight-bar .progress-bar-fill").css("width", firefight_bar_progress);
+
+    console.log(data[0]["custom_field_options"]);
   });
-  var backme_api = "https://taiwanbar.backme.tw/api/projects/219?token=02e8a88dbdc1bdb87f85cb48e82e9f9d";
-  $.getJSON( backme_api ).done(function(data){
-    var progress = data["money_pledged"] / data["money_goal"];
-    $(".progress-bar").css("width", progress)
-  });
+
 });
