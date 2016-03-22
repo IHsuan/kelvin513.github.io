b1e497cbcd2fc0add2a64458d78e485c72b2ce13
Kelvin Lin <kelvin@backer-founder.com>
2016-03-22 19:05:10 +0800

    money next_show

--- assets/js/parse-0321.js
+++ assets/js/parse-0321.js
@@ -2,7 +2,13 @@ $(function(){
   var progress_api = "https://taiwanbar.backme.tw/api/projects/219?token=02e8a88dbdc1bdb87f85cb48e82e9f9d";
   $.getJSON( progress_api ).done(function(data){
     var progress = data["money_pledged"] / data["money_goal"] * 100;
-    $(".progress-bar").css("width", progress+'%')
+    $(".progress-bar").css("width", progress+'%');
+
+    var already_succeed = parseInt(data["money_pledged"] / data["money_goal"])
+
+    var next_show = data["money_goal"] - (data["money_pledged"] % data["money_goal"])
+
+    $(".distance_parent").html("<img class='director-board' src='assets/img/導演板.svg'/>已成功集資 " + already_succeed +" 個節目！距離下一個節目還有<b> $"+ next_show + " </b>元");
   });
 
   var bar_api = "https://taiwanbar.backme.tw/special_partner/taiwanbar-conf/fields_count"
