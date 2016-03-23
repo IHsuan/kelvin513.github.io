b90c163051e92829fa82e4492829f436381344c1
Kelvin Lin <kelvin@backer-founder.com>
2016-03-23 10:32:17 +0800

    parse-0322.js update

--- assets/js/parse-0322.js
+++ assets/js/parse-0322.js
@@ -4,11 +4,11 @@ $(function(){
     var progress = data["money_pledged"] / data["money_goal"] * 100;
     $(".progress-bar").css("width", progress+'%');
 
-    var already_succeed = parseInt(data["money_pledged"] / data["money_goal"])
+    var already_succeed = parseInt(data["money_pledged"] / data["money_goal"]);
 
-    var next_show = data["money_goal"] - (data["money_pledged"] % data["money_goal"])
+    var next_show = data["money_goal"] - (data["money_pledged"] % data["money_goal"]);
 
-    $(".distance_parent").html("<img class='director-board' src='assets/img/導演板.svg'/>已成功集資 " + already_succeed +" 個節目！距離下一個節目還有<b> $"+ next_show + " </b>元");
+    $(".distance_parent").html("<img class='director-board' src='assets/img/導演板.svg'/>已成功集資 " + already_succeed +" 個節目！距離下一個節目還有<b> $" + next_show + " </b>元");
   });
 
   var bar_api = "https://taiwanbar.backme.tw/special_partner/taiwanbar-conf/fields_count"
