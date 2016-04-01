596acad2191b3e3f966fc67dc85e98b347ed22e3
Kelvin Lin <kelvin@backer-founder.com>
2016-04-01 09:56:04 +0800

    progress word WIP

--- assets/js/parse-0322.js
+++ assets/js/parse-0322.js
@@ -8,7 +8,8 @@ $(function(){
 
     var next_show = data["money_goal"] - (data["money_pledged"] % data["money_goal"]);
 
-    $(".distance_parent").html("<img class='director-board' src='assets/img/導演板.svg'/><span class='white-bg'>已成功集資 " + already_succeed +"/50 個節目！距離下一個節目還有<b> $" + next_show + " </b>元</span>");
+    $(".distance_parent").html("<img class='director-board white-bg' src='assets/img/導演板.svg'/><span class='white-bg'>已成功集資 " + already_succeed +"/50 個節目！距離下一個節目還有<b> $" + next_show + " </b>元</span>");
+    $(".distance_parent").html("已經有 " + 183 + " 人支持");
   });
 
   var bar_api = "https://taiwanbar.backme.tw/special_partner/taiwanbar-conf/fields_count"
