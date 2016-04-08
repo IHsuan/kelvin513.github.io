42a08620697a54872f49c48abb856a75359e20b8
Kelvin Lin <kelvin@backer-founder.com>
2016-04-08 19:34:44 +0800

    fancybox done

--- assets/js/fancybox.js
+++ assets/js/fancybox.js
@@ -1,30 +1,17 @@
-$(document).on('click', '#fancybox', function(){
+$(document).on('click', '.fancybox', function(){
 	$('#panel').css("display", "block");
-});
-$('#closeArea').bind('click',function(){
-	$('#panel').style.display = "none";
+	$('body').css("overflow", "hidden");
+	$('#infoContainer').addClass("fade-in");
 });
 
-var replaceClass = function(domObj , cName){
-	domObj.className = domObj.className.replace(cName , "");
-}
-var addClass = function(domObj , cName){
-	if( domObj.className.search(cName) !== -1 )
-		return;
-	domObj.className += ' ' + cName;
-}
-var open = false;
-$('#X').bind('click',function(){
-	if( open===false ){
-		$('#menu').style.display = "block";
-		setTimeout( function(){
-			addClass( $('#menu') , "menuOpen" );
-		} , 100 );
-		open = true;	
-	}
-	else{
-		$('#menu').style.display = "none";
-		replaceClass( $('#menu') , "menuOpen" );
-		open = false;	
-	}
-})
+$(document).on('click','#closeArea', function(){
+	$('#panel').css("display", "none");
+	$('#infoContainer').removeClass("fade-in");
+	$('body').css("overflow", "scroll");
+});
+
+$(document).on('click','#close', function(){
+	$('#panel').css("display", "none");
+	$('#infoContainer').removeClass("fade-in");
+	$('body').css("overflow", "scroll");
+});
